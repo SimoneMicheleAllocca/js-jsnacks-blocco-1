22394d8964c8352a024121e69a2a14330aab520a
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-04-02 20:17:04 +0200

    third snack

---
+++ jsnack9/README.md
@@ -0,0 +1,3 @@
+Calcola la somma e la media dei primi 10 numeri.
+- [x] somma dei primi 10 numeri
+- [x] media dei numeri
---
+++ jsnack9/script.js
@@ -0,0 +1,12 @@
+let somma = 0;
+
+for (let i = 1; i <= 10; i++) {
+    somma += i;
+}
+    console.log(somma);
+
+let media = somma / 10;
+    
+
+console.log("La somma è:", somma);
+console.log("La media è:", media);
