0a374002cd9ecbc228743d751f873f17bfe93016
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-04-02 19:26:07 +0200

    first snack

---
+++ jsnack1/README.md
@@ -0,0 +1,5 @@
+Il software deve chiedere per 10 volte all'utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+- [x] il programma chiede per dieci volte un numero all'utente
+- [x] somma tutti i numeri che ha dato l'utente
---
+++ jsnack1/script.js
@@ -0,0 +1,16 @@
+const num1 = parseInt(prompt("Dimmi un numero"));
+    console.log(num1, typeof num1);
+const num2 = parseInt(prompt("Dimmi un numero"));
+const num3 = parseInt(prompt("Dimmi un numero"));
+const num4 = parseInt(prompt("Dimmi un numero"));
+const num5 = parseInt(prompt("Dimmi un numero"));
+const num6 = parseInt(prompt("Dimmi un numero"));
+const num7 = parseInt(prompt("Dimmi un numero"));
+const num8 = parseInt(prompt("Dimmi un numero"));
+const num9 = parseInt(prompt("Dimmi un numero"));
+const num10 = parseInt(prompt("Dimmi un numero"));
+
+const somma = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+    console.log(somma);
+
+
